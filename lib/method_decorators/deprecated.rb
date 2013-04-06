faf20c6e6c13c0b1d4bfe97c49a6a8b1ed113aed
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-04-06 17:54:55 +0900

    Change name: Obsolete -> Deprecated

--- lib/method_decorators/deprecated.rb
+++ lib/method_decorators/deprecated.rb
@@ -5,38 +5,38 @@ module MethodDecorators
   #   class MyClass
   #     extend MethodDecorators
   #
-  #     +MethodDecorators::Obsolete
-  #     def obsolete_method
+  #     +MethodDecorators::Deprecated
+  #     def deprecated_method
   #       brand_new_method
   #     end
   #   end
-  # When obsolete_method is called, message
-  #   "MyClass#obsolete_method is obsolete"
+  # When deprecated_method is called, message
+  #   "MyClass#deprecated_method is deprecated"
   # is output.
   #
   # Another example:
   #   class MyClass
   #     extend MethodDecorators
   #
-  #     +MethodDecorators::Obsolete.new('obsolete_method will be removed in the future')
-  #     def obsolete_method
+  #     +MethodDecorators::Deprecated.new('deprecated_method will be removed in the future')
+  #     def deprecated_method
   #       brand_new_method
   #     end
   #   end
   # Above output given message
-  #   "obsolete_method will be removed in the future"
+  #   "deprecated_method will be removed in the future"
   #
   # Custom message example:
   #   class MyClass
   #     extend MethodDecorators
   #
-  #     +MethodDecorators::Obsolete.new {|class_name, method_name| "#{class_name}##{method_name} will be removed in the future. Use #{class_name}#brand_new_method instead"}
-  #     def obsolete_method
+  #     +MethodDecorators::Deprecated.new {|class_name, method_name| "#{class_name}##{method_name} will be removed in the future. Use #{class_name}#brand_new_method instead"}
+  #     def deprecated_method
   #       brand_new_method
   #     end
   #   end
   # Outputs
-  #   "MyClass#obsolete_method will be removed in the future. Use MyClass#brand_new_method instead"
+  #   "MyClass#deprecated_method will be removed in the future. Use MyClass#brand_new_method instead"
   # As you see, you can use class name as the first argument and method name as the second in the block.
   #
   # Formatter example:
@@ -49,21 +49,21 @@ module MethodDecorators
   #     extend MethodDecorators
   #
   #     formatter1 = ->(class_mane, method_name) {"#{class_name}##{method_name} will be removed in the next version. Use #{class_name}#brand_new_method instead"}
-  #     +MethodDecorators::Obsolete.new(formatter1)
+  #     +MethodDecorators::Deprecated.new(formatter1)
   #     def very_old_method
   #       brand_new_method
   #     end
   #
-  #     + MethodDecorators::Obsolete.new(Formatter2.new)
-  #     def obsolete_method
+  #     + MethodDecorators::Deprecated.new(Formatter2.new)
+  #     def deprecated_method
   #       brand_new_method
   #     end
   #   end
   # Outputs
-  #   "MyClass#obsolete_method will be removed in the future. Use MyClass#brand_new_method instead"
+  #   "MyClass#deprecated_method will be removed in the future. Use MyClass#brand_new_method instead"
   # You can give any object which responds to method "call" like Proc.
-  class Obsolete < Decorator
-    DEFAULT_FORMATTER = lambda {|class_name, method_name| "#{class_name}##{method_name} is obsolete"}
+  class Deprecated < Decorator
+    DEFAULT_FORMATTER = lambda {|class_name, method_name| "#{class_name}##{method_name} is deprecated"}
     def initialize(message=nil, &blk)
       @message = message || blk || DEFAULT_FORMATTER
     end
