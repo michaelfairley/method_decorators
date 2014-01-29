aac90e957c52a5d1a954bc23a5bf2a0564a878d0
Michael Fairley <michaelfairley@gmail.com>
2014-01-28 21:54:46 -0600

    Prevent different memoized methods in the same class from sharing a memos

--- lib/method_decorators/memoize.rb
+++ lib/method_decorators/memoize.rb
@@ -2,6 +2,10 @@ require "method_decorators"
 
 module MethodDecorators
   class Memoize < Decorator
+    def initialize
+      @memo_ivar = "@_memoize_cache#{rand(10**10)}"
+    end
+
     def call(orig, this, *args, &blk)
       return cache(this)[args] if cache(this).has_key?(args)
       cache(this)[args] = orig.call(*args, &blk)
@@ -9,8 +13,9 @@ module MethodDecorators
 
     private
     def cache(this)
+      memo_ivar = @memo_ivar
       this.instance_eval do
-        return @_memoize_cache ||= {}
+        instance_variable_get(memo_ivar) || instance_variable_set(memo_ivar, {})
       end
     end
   end
