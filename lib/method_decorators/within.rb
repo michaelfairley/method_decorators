192ef42a1c1a4c7f2dbf02ec72a84c677dcd9119
Toby Hsieh <thrasher13b@gmail.com>
2014-12-25 19:36:23 -0800

    Allow specific exception for Within

--- lib/method_decorators/within.rb
+++ lib/method_decorators/within.rb
@@ -3,12 +3,13 @@ require 'timeout'
 
 module MethodDecorators
   class Within < Decorator
-    def initialize(timeout)
+    def initialize(timeout, exception_class = nil)
       @seconds = timeout
+      @exception_class = exception_class
     end
 
     def call(orig, this, *args, &blk)
-      Timeout::timeout(@seconds) do
+      Timeout.timeout(@seconds, @exception_class) do
         orig.call(*args, &blk)
       end
     end
